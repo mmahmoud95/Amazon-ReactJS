cd23c715d373aa51e2ed18c9b5576b0784f26ec2
Hamza-mohamed <hamzamohamed20201@gmail.com>
2023-10-26 22:35:57 +0300

    adding product details page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Register from "./pages/Register/Register";
 import Login from "./pages/Login.jsx/Login";
 import Applayout from "./components/layout/AppLayout/applayout";
 import { Category } from "./pages/category/category";
-import { ProductDetails } from "./pages/product-details/product-details";
+import ProductDetails from "./pages/product-details/ProductDetails";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
             { index: true, element: <Home /> },
             { path: "cart", element: <Cart /> },
             { path: "products/category/:categoryname", element: <Category /> },
-            { path: "products/:id", element: <ProductDetails /> },
+            { path: "products/:id", element: <ProductDetails/> },
         ],
     },
     { path: "login", element: <Login /> },

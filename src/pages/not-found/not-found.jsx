db8b55c457d48a0a43e4c2acb2111965a024293b
Hamza-mohamed <hamzamohamed20201@gmail.com>
2023-12-04 03:49:40 +0200

    ..update not found

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -5,17 +5,15 @@ const NotFound = () => {
   return (
     <div>
       <div className="d-flex flex-column justify-content-center align-items-center bg-dark text-white vh-100">
-        <h1 className="display-1 font-weight-bold">404</h1>
-        <div className="bg-danger px-2 text-sm rounded rotate-12 position-absolute">
+        <h1 className="display-1 font-weight-bold py-2">404</h1>
+        <div className="bg-danger px-2 text-sm rounded rotate-12 position-absolute " >
           Page Not Found
         </div>
-        <button className="mt-5">
-          <Link to="/" className="btn btn-outline-danger">
-            <span className="position-relative d-inline-block px-4 py-2 border border-dark">
+          <Link to="/" className="btn bg-warning py-3">
+            <span className="position-relative d-inline-block px-4 py-2 ">
               Go Home
             </span>
           </Link>
-        </button>
       </div>
     </div>
   );
